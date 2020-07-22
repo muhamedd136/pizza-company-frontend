a93d8d30c69397fcc5725e577734ace01f25c111
“Muhamed <muhamed.delihasanovic@stu.ibu.edu.ba>
2020-07-22 21:25:12 +0200

    favicon fix

---
+++ build/precache-manifest.affea84e2bf3b8a39b28e678df4a0167.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91f5f0a95688c409b23d1e08ba1f69ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9824bcf63695ebd5f323",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "2ae517a308035df3db8f",
+    "url": "/static/css/main.4820b547.chunk.css"
+  },
+  {
+    "revision": "9824bcf63695ebd5f323",
+    "url": "/static/js/2.4e62e9b0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.4e62e9b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ae517a308035df3db8f",
+    "url": "/static/js/main.2a783a75.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "621ac9dad3a1c85f41149cfb936245b5",
+    "url": "/static/media/shopping-bag-1.621ac9da.svg"
+  }
+]);
